99e88053af464bec5cd8c19d0c00f2903e1dbcf5
Carlos Eduardo Millani <carloseduardomillani@gmail.com>
2016-05-13 20:12:15 -0300

    Add files via upload

---
+++ exp9a/FlipFlopD.vhd
@@ -0,0 +1,22 @@
+library ieee;
+USE ieee.std_logic_1164.all;
+ENTITY FlipFlopD IS
+	GENERIC(n : NATURAL := 4);
+	PORT ( D : IN STD_LOGIC_VECTOR(n-1 downto 0);
+		clk, rst : IN STD_LOGIC ;
+		Q : OUT STD_LOGIC_VECTOR(n-1 downto 0));
+END FlipFlopD;
+
+ARCHITECTURE ffD_comp OF FlipFlopD IS
+	BEGIN
+		PROCESS (clk, rst)
+		VARIABLE Qvar : STD_LOGIC_VECTOR(n-1 downto 0);
+		BEGIN
+			IF rst = '1' THEN
+				Qvar := (OTHERS => '0');
+			ELSIF ( clk'event and clk = '1' ) THEN
+				Qvar := D;
+			END IF ;
+			Q <= Qvar;
+	END PROCESS ;
+END ffD_comp ;
